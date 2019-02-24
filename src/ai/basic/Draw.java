62b6972addb69ee15df85de9c1356886e39619e8
Pranav-Badrinathan <40524763+Pranav-Badrinathan@users.noreply.github.com>
2019-02-24 13:55:40 -0500

    Changed Draw Class
    The Draw class now calls the method in IHasToBeDrawn to draw object to
    screen

--- src/ai/basic/Draw.java
+++ src/ai/basic/Draw.java
@@ -4,23 +4,25 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-import ai.basic.ai_components.Dot;
+import ai.basic.util.interfaces.IHasToBeDrawn;
 
 public class Draw extends JPanel 
 {
 	private static final long serialVersionUID = 1458492569258787300L;
 	
-	private Dot dot;
+	private IHasToBeDrawn[] itemsToDraw;
 	
-	public Draw(Dot dot)
+	public Draw(IHasToBeDrawn[] itemsToDraw)
 	{
-		this.dot = dot;
+		this.itemsToDraw = itemsToDraw;
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) 
 	{
-		g.setColor(dot.dotColor);
-		g.fillOval((int)dot.position.x, (int)dot.position.y, dot.size, dot.size);
+		for (IHasToBeDrawn item : itemsToDraw)
+		{
+			item.drawToScreen(g);
+		}
 	}
 }
